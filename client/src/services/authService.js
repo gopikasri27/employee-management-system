4fde183f8f54b68269226207f3f54937a542ccd0
gopikasri27 <gopika27raj@gmail.com>
2026-05-26 10:32:39 +0530

    updated backend url

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "http://https://employee-management-system-production-ce83.up.railway.app/api/auth";
 
 export const loginUser = (userData) =>
   axios.post(`${API_URL}/login`, userData);
